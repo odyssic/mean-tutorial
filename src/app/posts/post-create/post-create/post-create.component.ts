16f4e90a076a71e2817c10880262e7ad167e88cc
odyssic <stevenbuddenjr@gmail.com>
2021-03-01 14:25:55 -0500

    working with initial post form

---
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent {
+  onAddPost() {
+    alert('Post Added');
+  }
+}
