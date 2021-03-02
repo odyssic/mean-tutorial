c02e03a25813700958aabca520d56903e5783295
odyssic <stevenbuddenjr@gmail.com>
2021-03-02 12:47:51 -0500

    form added, model created

--- src/app/posts/post-create/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create/post-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Post } from './../../post.model';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'No Content';
+  enteredContent = '';
+  enteredTitle = '';
+  @Output()
+  postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content,
+    };
+    this.postCreated.emit(post);
   }
 }
--- src/app/posts/post-list/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list/post-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Post } from './../../post.model';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PostListComponent {
+  @Input() posts: Post[] = [];
+  // { title: 'First Post', content: 'This is the first post' },
+  // { title: 'Second Post', content: 'This is the second post' },
+  // { title: 'Third Post', content: 'This is the third post' },
 }
