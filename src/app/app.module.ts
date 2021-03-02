d3f342995efc3b64d3bc4992ad7d72f46bc66a39
odyssic <stevenbuddenjr@gmail.com>
2021-03-02 11:26:22 -0500

    mat expansion module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,29 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create/post-create.component';
 import { HeaderComponent } from './header/header/header.component';
+import { PostCreateComponent } from './posts/post-create/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list/post-list.component';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
+    MatExpansionModule,
     MatInputModule,
     MatSliderModule,
     MatToolbarModule,
